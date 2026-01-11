46b5b33684ba5306215d8b8ce403b384c0c7ddb4
Linganna <linganna@gmail.com>
2026-01-10 22:45:51 -0600

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,19 +1,42 @@
-# Uncomment the imports before you add the code
-from django.urls import path
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
 from . import views
 
-app_name = 'djangoapp'
+app_name = "djangoapp"
 
 urlpatterns = [
-    # # path for registration
+    # Path for registration
+    path("register", views.registration, name="registration"),
 
-    # path for login
-    path(route='login', view=views.login_user, name='login'),
+    # Path for login
+    path("login", views.login_user, name="login"),
 
-    # path for dealer reviews view
+    # Path for logout
+    path("logout", views.logout_request, name="logout"),
 
-    # path for add a review view
+    # Cars
+    path("get_cars", views.get_cars, name="getcars"),
 
+    # Dealers
+    path("get_dealers/", views.get_dealerships, name="get_dealers"),
+    path(
+        "get_dealers/<str:state>",
+        views.get_dealerships,
+        name="get_dealers_by_state",
+    ),
+    path(
+        "dealer/<int:dealer_id>",
+        views.get_dealer_details,
+        name="dealer_details",
+    ),
+    path(
+        "reviews/dealer/<int:dealer_id>",
+        views.get_dealer_reviews,
+        name="dealer_reviews",
+    ),
+
+    # Add a review
+    path("add_review", views.add_review, name="add_review"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
