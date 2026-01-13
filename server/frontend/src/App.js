3ffbfcbc3111c61e27fb1a34d39692fdcb50604a
Linganna <linganna@gmail.com>
2026-01-12 20:04:33 -0600

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,17 +1,22 @@
-import LoginPanel from "./components/Login/Login"
-import Register from "./components/Register/Register";
+import { Route, Routes } from "react-router-dom";
+import Dealer from "./components/Dealers/Dealer";
 import Dealers from './components/Dealers/Dealers';
-import Dealer from "./components/Dealers/Dealer"
-import PostReview from "./components/Dealers/PostReview"
-import { Routes, Route } from "react-router-dom";
+import PostReview from "./components/Dealers/PostReview";
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPanel />} />
+      {/* Register */}
       <Route path="/register" element={<Register />} />
+      {/* Login */}
+      <Route path="/login" element={<LoginPanel />} />
+      {/* Dealers */}
       <Route path="/dealers" element={<Dealers/>} />
+      {/* Dealer */}
       <Route path="/dealer/:id" element={<Dealer/>} />
+      {/* Post Review */}
       <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
